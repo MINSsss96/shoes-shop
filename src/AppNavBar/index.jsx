5b8988a034830a90559d7b00bef0735bb0a53a32
MINSsss96 <minsue0422@naver.com>
2025-09-16 16:40:30 +0900

    react-router-dom 설치 및 셋팅

--- src/AppNavBar/index.jsx
+++ src/AppNavBar/index.jsx
@@ -1,28 +1,48 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import './AppNavBar.css'
+// Route : 페이지 이동 처리
+// Routes : url에 담겨있는 정보를 획득
+// Link : 실제로 페이지를 보여주는 역할, Link 위치에 컴포넌트 뿌려줌
+import { Route, Routes, Link } from 'react-router-dom';
 
 
-function AppNavBar(){
-return(
-  <>
+function AppNavBar() {
+  return (
+    <>
       <>
-      <div className='app'>
+        <div className='app'>
           <Navbar bg="dark" data-bs-theme="dark">
-          <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-      </div>
-    </>
+            <Container>
+              <Navbar.Brand>Muzunjang</Navbar.Brand>
+              <Nav className="me-auto">
+                <Nav.Link>
+                  <Link to={"/"}>Home</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link to={"/detail"}>Detail</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link to={"/cart"}>Cart</Link>
+                </Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
 
-  </>
-)
+          {/* Routing 정보를 한꺼번에 모아놓는 장소 */}
+          {/* 스프링에서 사용하는 컨트롤러 클래스 */}
+
+          <Routes>
+            <Route path="/" element={<div>메인페이지</div>} />
+            <Route path="/detail" element={<div>상세페이지</div>} />
+            <Route path="/cart" element={<div>장바구니페이지</div>} />
+
+          </Routes>
+        </div>
+      </>
+
+    </>
+  )
 
 }
 export default AppNavBar;
