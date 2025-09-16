2e96ccc186a93c8ce233a8ef51233bbf34fd9e2a
MINSsss96 <minsue0422@naver.com>
2025-09-16 17:26:40 +0900

    NavBar 및  Route 작업 완료

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import { Container, Row, Col } from 'react-bootstrap';
 import data from "./data/data";
 import { useState } from "react";
 import Product from "./Product";
+import { Routes, Route } from "react-router-dom";
+import Detail from "./Detail";
+
 
 function App() {
   // 상품정보를 갖는 product 스테이트를 만든다.
   const [product, setProduct] = useState(data);
-  console.log(product[0].title)
 
   return (
     <>
@@ -19,18 +21,30 @@ function App() {
       <AppNavBar />
       {/* 네비게이션 바 영역 끝 */}
 
+
+
       {/* 메인 대문사진 영역 시작 */}
       <div className="main-bg"
         style={{ backgroundImage: `url('${bg_png}')` }}
       />
       {/* 메인 대문사진 영역 끝 */}
+
+                      {/* Routing 정보를 한꺼번에 모아놓는 장소 */}
+          {/* 스프링에서 사용하는 컨트롤러 클래스 */}
+
+          <Routes>
+            <Route path="/" element={<div>메인페이지</div>} />
+            <Route path="/detail" element={<Detail />} />
+            <Route path="/cart" element={<div>장바구니페이지</div>} />
+            <Route path="*" element={<div>Page Not Found 404 Error</div>}></Route>
+          </Routes>
       {/* 상품진열영역 시작 */}
 
       {/* 상품진열영역 끝 */}
       <Container>
         <Row>
           {
-            product.map((shoes, _) => {
+            product.map((shoes, _)=>{
               return (
                 <Col key={shoes.id} className="text-center">
                   <Product shoes={shoes} />
--- src/AppNavBar/index.jsx
+++ src/AppNavBar/index.jsx
@@ -4,10 +4,14 @@ import './AppNavBar.css'
 // Route : 페이지 이동 처리
 // Routes : url에 담겨있는 정보를 획득
 // Link : 실제로 페이지를 보여주는 역할, Link 위치에 컴포넌트 뿌려줌
-import { Route, Routes, Link } from 'react-router-dom';
-
+import { Link, useNavigate } from 'react-router-dom';
+import Detail from '../Detail';
 
 function AppNavBar() {
+
+  const navigate = useNavigate();
+
+
   return (
     <>
       <>
@@ -16,28 +20,14 @@ function AppNavBar() {
             <Container>
               <Navbar.Brand>Muzunjang</Navbar.Brand>
               <Nav className="me-auto">
-                <Nav.Link>
-                  <Link to={"/"}>Home</Link>
-                </Nav.Link>
-                <Nav.Link>
-                  <Link to={"/detail"}>Detail</Link>
-                </Nav.Link>
-                <Nav.Link>
-                  <Link to={"/cart"}>Cart</Link>
-                </Nav.Link>
+                <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
+                <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
+                <Nav.Link>Cart</Nav.Link>
               </Nav>
             </Container>
           </Navbar>
 
-          {/* Routing 정보를 한꺼번에 모아놓는 장소 */}
-          {/* 스프링에서 사용하는 컨트롤러 클래스 */}
 
-          <Routes>
-            <Route path="/" element={<div>메인페이지</div>} />
-            <Route path="/detail" element={<div>상세페이지</div>} />
-            <Route path="/cart" element={<div>장바구니페이지</div>} />
-
-          </Routes>
         </div>
       </>
 
