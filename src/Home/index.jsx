d96583576d9b37e4e718905234ace6b6291f335c
MINSsss96 <minsue0422@naver.com>
2025-09-18 16:52:23 +0900

    axios로 추가 상품 가져오기

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,18 +1,19 @@
-import {Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Product from '../Product';
+import axios from 'axios';
 
-function Home({product}){
-  return(
+function Home({ product, setProduct }) {
+  return (
     <div>
       {/* 상품진열영역 시작 */}
       <Container>
-        <Row>
+        <Row xs={3}>
           {
-            product.map((shoes, _)=>{
-              return(
+            product.map((shoes, _) => {
+              return (
                 <Col key={shoes.id} className="text-center">
                   {/* Product 콤포넌트 자리 */}
-                  <Product shoes={shoes}/>
+                  <Product shoes={shoes} />
                 </Col>
               )
             })
@@ -20,6 +21,36 @@ function Home({product}){
         </Row>
       </Container>
       {/* 상품진열영역 끝 */}
+      <div className='d-flex justify-content-center
+          align-items-center'>
+        <button variant='primary' size="lg"
+          onClick={async() => {
+            try {
+              const result1 = await axios('https://zzzmini.github.io/js/react_data_01.json')
+              let temp = [...product, ...result1.data];
+              setProduct(temp);
+            } catch (error) {
+              console.log("가져오기 실패", error)
+            }
+
+
+            // 버튼을 클릭하면 데이터 3개를 가져오는 함수
+            // axios
+            // .get('https://zzzmini.github.io/js/react_data_01.json')
+            // .then((result)=>{
+            //   let temp = [... product]
+            //   for(let x of result.data){
+            //     temp.push(x)
+            //   }
+            //   setProduct(temp)
+            // })
+            // .catch(()=>{
+            //   console.log('가져오기 실패')
+            // })
+          }}>
+          데이터 가져오기
+        </button>
+      </div>
     </div>
   )
 }
