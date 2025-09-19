04eea2cfd6a6122ca01f909360af84eb75908920
MINSsss96 <minsue0422@naver.com>
2025-09-19 10:17:54 +0900

    탭 추가 작업 및 애니메이션

--- src/TabContent/index.jsx
+++ src/TabContent/index.jsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react";
+
 function TabContent({ tabState }) {
+
+  // fade 값으로 css를 지정하도록 함
+  let [fade, setFade] = useState('')
+
+  // useEffect로 타이머를 실행
+  // 0.2초 후에 fade = ani_end 로 바꿔줌
+  useEffect(()=>{
+    let timer = setTimeout(()=>{
+      setFade('ani_end')
+    }, 100)
+    return(()=>{
+      clearTimeout(timer);
+      setFade('')
+    })
+  }, [tabState]
+)
+
+
+  
   return (
-    [
+    <div className= {`ani_start ${fade}`}>
+         {[
       <div> 내용1</div>,
       <div> 내용2</div>,
       <div> 내용3</div>,
-    ][tabState]
+    ][tabState]}
+    </div>
   )
 
 }
