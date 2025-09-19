626952a20fde0512627cfccf1c7d7da37eaef668
MINSsss96 <minsue0422@naver.com>
2025-09-19 11:51:10 +0900

    여러가지 탭 추가

--- src/TabInfo/DetailInfo.jsx
+++ src/TabInfo/DetailInfo.jsx
@@ -1,13 +1,15 @@
 import { productFeatures } from "../data/productFeatures";
 
-export default function DetailInfo() {
+export default function DetailInfo({product}) {
   const features = productFeatures[product?.id] ?? productFeatures.default;
 
   return (
     <div className="section-card mt-3">
       <h5>이 제품이 좋은 이유</h5>
-      <ul>
-        {/* productFeatures.js에서 가져와서 처리 */}
+     <ul>
+        {features.map((feature, idx) => (
+          <li key={idx}>{feature}</li>
+        ))}
       </ul>
 
       <p className="muted small mb-0">* 사용자 모니터/발형에 따라 체감이 다를 수 있어요.</p>
